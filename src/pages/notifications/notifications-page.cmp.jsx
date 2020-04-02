4a23d799845b5a0e7bd09212f6b1a9c62170aab0
iliyan <iliyan.tsachev@ilnweb.com>
2020-04-02 15:18:49 +0200

    refactoring notifications page and adding a back button

--- src/pages/notifications/notifications-page.cmp.jsx
+++ src/pages/notifications/notifications-page.cmp.jsx
@@ -6,6 +6,7 @@ import { selectCurrentUser } from '../../redux/user/user.selectors';
 import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
 import { acceptInvitation } from '../../firebase/firebase.config';
+import { MdArrowBack } from "react-icons/md";
 
 class NotificationsPage extends React.Component {
 
@@ -17,8 +18,9 @@ class NotificationsPage extends React.Component {
   render() {
     const { currentUser } = this.props;
 		return (
-			<div className="notifications-page">
-				<HeaderContainer>
+      <div className="notifications-page">
+        <HeaderContainer>
+        <MdArrowBack className="back-button" onClick={()=>this.props.history.goBack()}/>
 					<h1>Your notifications</h1>
 				</HeaderContainer>
 				<div className="notifications-container m-10">
