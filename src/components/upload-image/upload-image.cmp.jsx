86aca86aa800a83037e975c30e18600153af6261
iliyan <iliyan.tsachev@ilnweb.com>
2020-04-08 12:53:08 +0200

    adding better id generator for image upload

--- src/components/upload-image/upload-image.cmp.jsx
+++ src/components/upload-image/upload-image.cmp.jsx
@@ -4,87 +4,95 @@ import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
 import { storageFB } from '../../firebase/firebase.config';
 
 function getBase64(img, callback) {
-  const reader = new FileReader();
-  reader.addEventListener('load', () => callback(reader.result));
-  reader.readAsDataURL(img);
+	const reader = new FileReader();
+	reader.addEventListener('load', () => callback(reader.result));
+	reader.readAsDataURL(img);
 }
 
 function beforeUpload(file) {
-  const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
-  if (!isJpgOrPng) {
-    message.error('You can only upload JPG/PNG file!');
-  }
-  const isLt2M = file.size / 1024 / 1024 < 2;
-  if (!isLt2M) {
-    message.error('Image must smaller than 2MB!');
-  }
-  return isJpgOrPng && isLt2M;
+	const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+	if (!isJpgOrPng) {
+		message.error('You can only upload JPG/PNG file!');
+	}
+	const isLt2M = file.size / 1024 / 1024 < 2;
+	if (!isLt2M) {
+		message.error('Image must smaller than 2MB!');
+	}
+	return isJpgOrPng && isLt2M;
 }
 
 class UploadImage extends React.Component {
-  state = {
-    loading: false,
-  };
+	state = {
+		loading: false
+	};
 
-  handleChange = info => {
-    if (info.file.status === 'uploading') {
-      this.setState({ loading: true });
-      return;
-    }
-    if (info.file.status === 'done') {
-      // Get this url from response in real world.
-     
-      getBase64(info.file.originFileObj, imageUrl =>
-        this.setState({
-          imageUrl,
-          loading: false,
-        }, () => {
-            const imagesplit = imageUrl.split('/').slice(3, 4);
-            
-            const imageID = imagesplit[0].split('').slice(0, 30).join('');
-            const uploadTask = storageFB.ref(`/connectionimages/${imageID}`).put(info.file.originFileObj)
-            uploadTask.on('state_changed',
-              (snapShot) => {
-                //takes a snap shot of the process as it is happening
-                console.log(snapShot)
-              }, (err) => {
-                //catches the errors
-                console.log(err)
-              }, () => {
-                // gets the functions from storage refences the image storage in firebase by the children
-                // gets the download url then sets the image from firebase as the value for the imgUrl key:
-                storageFB.ref('connectionimages').child(imageID).getDownloadURL()
-                  .then(fireBaseUrl => {
-                    this.props.handleImage(fireBaseUrl);
-                  })
-              });
-      }),
-      );
-    }
-  };
+	handleChange = (info) => {
+		if (info.file.status === 'uploading') {
+			this.setState({ loading: true });
+			return;
+		}
+		if (info.file.status === 'done') {
+			// Get this url from response in real world.
 
-  render() {
-    const uploadButton = (
-      <div>
-        {this.state.loading ? <LoadingOutlined /> : <PlusOutlined />}
-        <div className="ant-upload-text">Upload</div>
-      </div>
-    );
-    const { imageUrl } = this.state;
-    return (
-      <Upload
-        name="avatar"
-        listType="picture-card"
-        className="avatar-uploader"
-        showUploadList={false}
-        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-        beforeUpload={beforeUpload}
-        onChange={this.handleChange}
-      >
-        {imageUrl ? <img src={imageUrl} alt="avatar" style={{ width: '100%' }} /> : uploadButton}
-      </Upload>
-    );
-  }
+			getBase64(info.file.originFileObj, (imageUrl) =>
+				this.setState(
+					{
+						imageUrl,
+						loading: false
+					},
+					() => {
+						const id = () => {
+							return '_' + Math.random().toString(36).substr(2, 9) + '-' + Math.random().toString(36).substr(2, 9);
+						};
+						const imageID = id();
+
+						const uploadTask = storageFB.ref(`/connectionimages/${imageID}`).put(info.file.originFileObj);
+						uploadTask.on(
+							'state_changed',
+							(snapShot) => {
+								//takes a snap shot of the process as it is happening
+								console.log(snapShot);
+							},
+							(err) => {
+								//catches the errors
+								console.log(err);
+							},
+							() => {
+								// gets the functions from storage refences the image storage in firebase by the children
+								// gets the download url then sets the image from firebase as the value for the imgUrl key:
+								storageFB.ref('connectionimages').child(imageID).getDownloadURL().then((fireBaseUrl) => {
+									this.props.handleImage(fireBaseUrl);
+								});
+							}
+						);
+					}
+				)
+			);
+		}
+	};
+
+	render() {
+		const uploadButton = (
+			<div>
+				{this.state.loading ? <LoadingOutlined /> : <PlusOutlined />}
+				<div className="ant-upload-text">Upload</div>
+			</div>
+		);
+		const { imageUrl } = this.state;
+		return (
+			<Upload
+				name="avatar"
+				listType="picture-card"
+				className="avatar-uploader"
+				showUploadList={false}
+				action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+				beforeUpload={beforeUpload}
+				onChange={this.handleChange}
+			>
+				{imageUrl ? <img src={imageUrl} alt="avatar" style={{ width: '100%' }} /> : uploadButton}
+			</Upload>
+		);
+	}
 }
 
 export default UploadImage;
