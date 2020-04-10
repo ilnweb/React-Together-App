509330672017e3d28f2fb0c3d565d9fa8b96ef06
iliyan <iliyan.tsachev@ilnweb.com>
2020-04-10 13:46:30 +0200

    adding enzyme, updating npm packs, fixing react scripts multiple errors

--- src/components/sign-in/sign-in.cmp.jsx
+++ src/components/sign-in/sign-in.cmp.jsx
@@ -36,7 +36,8 @@ class SignIn extends React.Component {
 		return (
 			<div className="sign-in-up flex-c-c">
 				<h1>Sign In</h1>
-				<Form className="sign-in-up flex-c">
+        <Form className="sign-in-up flex-c">
+          <label for="email1">Email</label>
 					<Input
 						id="email1"
 						name="email"
@@ -49,7 +50,8 @@ class SignIn extends React.Component {
 						suffix={<MdMailOutline style={{ color: 'rgba(0,0,0,.25)' }} />}
 						autoComplete="true"
 						onChange={this.handleChange}
-					/>
+          />
+          <label for="password1">Password</label>
 					<Input.Password
 						id="password1"
 						name="password"
@@ -62,18 +64,20 @@ class SignIn extends React.Component {
 						autoComplete="true"
 						onChange={this.handleChange}
 					/>
-					<Button className="button-size" size="large" type="primary" onClick={this.handleSubmit}>
+					<Button className="button-size"  size="large" type="primary" onClick={this.handleSubmit}>
 						Sign In
 					</Button>
 					<div className="flex-c input-style">
 						<Button
-							className="flex-c g-signin mr-5"
+              className="flex-c g-signin mr-5"
+              aria-label="Google log-in"
 							size="large"
               icon={<FaGoogle/>}
 							onClick={signInWithGoogle}
 						/>
 						<Button
-							className="flex-c f-signin ml-5"
+              className="flex-c f-signin ml-5"
+              aria-label="Facebook log-in"
 							size="large"
               icon={<FaFacebook/>}
 							onClick={signInWithFacebook}
