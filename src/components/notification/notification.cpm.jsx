91905890bdc33a5bf2bf2784bc0d49182299bde1
iliyan <iliyan.tsachev@ilnweb.com>
2020-05-05 18:22:50 +0200

    notification style update

--- src/components/notification/notification.cpm.jsx
+++ src/components/notification/notification.cpm.jsx
@@ -7,25 +7,31 @@ import { Link } from 'react-router-dom';
 const Notification = ({ item, handleAccept }) => {
 	return (
 		<div className="notification">
-      <div className="notification-data flex-c p-10">
-        <Avatar shape={item.type ? '' : 'square'} size={64} src={item.type ? item.userImg : item.connectionImg}/>
+			<div className="notification-data flex-c p-10">
+				<div>
+					<Avatar shape={item.type ? '' : 'square'} size="large" src={item.type ? item.userImg : item.connectionImg} />
+				</div>
 				<div className="flex-c-c ml-10">
-					<div className="notification-list-item ml-10 mt-10 flex-c-c" >
+					<div className="notification-list-item ml-10 mt-10 flex-c-c">
 						<div className="notification-date">
-							<strong>{item.displayName}</strong>&nbsp; {item.type ? item.notificationBody : 'invited you to connection' }&nbsp; <strong>{item.connectionName}</strong>
+							<strong>{item.displayName}</strong>{' '}
+							{item.type ? item.notificationBody : 'invited you to connection'}{' '}
+							<strong>{item.connectionName}</strong>
 						</div>
-						<Moment className="date-color" fromNow date={item.createdAt&&item.createdAt.toDate()} />
+						<Moment className="date-color" fromNow date={item.createdAt && item.createdAt.toDate()} />
 					</div>
-					{!item.type && <div className="notification-buttons flex">
-						<Button className="m-10" type="danger" size="large">
-              Decline
-            </Button>
-            <Link to="/all-connections">
-						<Button className="m-10" type="primary" size="large" onClick={() => handleAccept(item)}>
-							Accept
-            </Button>
-            </Link>
-					</div>}
+					{!item.type && (
+						<div className="notification-buttons flex">
+							<Button className="m-10" type="danger" size="large">
+								Decline
+							</Button>
+							<Link to="/all-connections">
+								<Button className="m-10" type="primary" size="large" onClick={() => handleAccept(item)}>
+									Accept
+								</Button>
+							</Link>
+						</div>
+					)}
 				</div>
 			</div>
 		</div>
