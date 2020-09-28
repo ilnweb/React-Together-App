3f6b5a5a45f7a54f6a944d949f5c451b0916db39
iliyan <iliyan.tsachev@ilnweb.com>
2020-09-28 11:30:50 +0200

    icon change

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import 'antd/dist/antd.css';
 import './style/css-util.css';
 import './style/sass-variable.scss';
-import './App.css';
+import './App.scss';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import SignInAndSignUpPage from './pages/signIn-signUp/signIn-signUp-page.cmp/signIn-signUp-page.cmp';
+// import SignInAndSignUpPage from './pages/signIn-signUp/signIn-signUp-page.cmp/signIn-signUp-page.cmp';
 import Header from './components/header/header.cmp';
-import SpendingPage from './pages/Spendings/spendings-page.cmp';
-import ConnectionsPage from './pages/conections/connections-page.cmp';
-import CalendarPage from './pages/calendar/calendar-page.cmp';
-import ToDoPage from './pages/todo/todo-page.com';
-import NotificationsPage from './pages/notifications/notifications-page.cmp';
-import AllConectionsPage from './pages/all-conections/all-connections-page.cmp';
-import UserProfile from './pages/user-profile/user-profile.cmp';
+// import SpendingPage from './pages/Spendings/spendings-page.cmp';
+// import ConnectionsPage from './pages/conections/connections-page.cmp';
+// import CalendarPage from './pages/calendar/calendar-page.cmp';
+// import ToDoPage from './pages/todo/todo-page.com';
+// import NotificationsPage from './pages/notifications/notifications-page.cmp';
+// import AllConectionsPage from './pages/all-conections/all-connections-page.cmp';
+// import UserProfile from './pages/user-profile/user-profile.cmp';
 import BottomNav from './components/bottom-nav/bottom-nav.cmp';
 import LoadingScreen from './components/loading-screen/loading-screen.cmp';
 import { authFB, createUserProfileDocument, pullConnection } from './firebase/firebase.config';
@@ -22,6 +22,15 @@ import { setConnection } from './redux/connection/connection.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { createStructuredSelector } from 'reselect';
 import WithSpinner from './components/with-spinner/with-spinner.cmp';
+
+const SignInAndSignUpPage = lazy(() => import('./pages/signIn-signUp/signIn-signUp-page.cmp/signIn-signUp-page.cmp'));
+const SpendingPage = lazy(() => import('./pages/Spendings/spendings-page.cmp'));
+const ConnectionsPage = lazy(() => import('./pages/conections/connections-page.cmp'));
+const CalendarPage = lazy(() => import('./pages/calendar/calendar-page.cmp'));
+const ToDoPage = lazy(() => import('./pages/todo/todo-page.com'));
+const NotificationsPage = lazy(() => import('./pages/notifications/notifications-page.cmp'));
+const AllConectionsPage = lazy(() => import('./pages/all-conections/all-connections-page.cmp'));
+const UserProfile = lazy(() => import('./pages/user-profile/user-profile.cmp'));
 
 
 const SpendingPageWithSpinner = WithSpinner(SpendingPage);
@@ -80,7 +89,8 @@ class App extends React.Component {
 					isLoading ? <LoadingScreen img="https://res.cloudinary.com/ilnphotography/image/upload/v1582856305/HomePage/undraw_mobile_marketing_iqbr_bznozj.svg" title="Lets do it better!" />:''
 				}
         <Header location={this.props.location}/>
-				<Switch>
+        <Switch>
+        <Suspense fallback={<LoadingScreen img="https://res.cloudinary.com/ilnphotography/image/upload/v1582856305/HomePage/undraw_mobile_marketing_iqbr_bznozj.svg" title="Lets do it better!" />}>
 					<Route
 						exact
 						path="/"
@@ -101,7 +111,8 @@ class App extends React.Component {
 					<Route path="/profile" component={UserProfile} />
 					<Route path="/to-do" component={ToDoPage} />
 					<Route path="/all-connections" component={AllConectionsPage} />
-					<Route path="/notifications" component={NotificationsPage} />
+          <Route path="/notifications" component={NotificationsPage} />
+          </Suspense>
 				</Switch>
 				<BottomNav />
 			</div>
