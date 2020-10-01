8f744c153838bde91a36fd80dda5a6bed4550627
iliyan <iliyan.tsachev@ilnweb.com>
2020-10-01 20:14:56 +0200

    updating loading screen

--- src/components/loading-screen/loading-screen.cmp.jsx
+++ src/components/loading-screen/loading-screen.cmp.jsx
@@ -1,24 +1,11 @@
 import React from 'react';
 import './loading-screen.scss';
-import { Button } from 'antd';
-import { Link } from 'react-router-dom';
 
-const LoadingScreen = ({ title, button, img, isLoading, inside ,mainTitle}) => {
-	return (
-		<div className={`flex-c-c isLoading ${inside && 'inside'}`}>
-			<div className="logo-container flex-c-c">
-				<h1 className="logo-2">{mainTitle}</h1>
-				<p>{title}</p>
-				{button && (
-					<Link to="/all-connections">
-						<Button type="primary" shape="round" size="large" className="mt-15">
-							Create new Group
-						</Button>
-					</Link>
-				)}
-				<img src={img} alt="" />
-			</div>
-		</div>
+const LoadingScreen = (isLoading) => {
+  return (
+    <div className="loading-screen flex-c-c">
+      <div className="spinner" />
+    </div>
 	);
 };
 
