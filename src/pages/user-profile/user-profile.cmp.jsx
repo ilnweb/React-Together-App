64efdb46550b0226b8cfa6220576a11f0b1f6e77
iliyan <iliyan.tsachev@ilnweb.com>
2020-10-02 15:59:22 +0200

    updating avatar reusability

--- src/pages/user-profile/user-profile.cmp.jsx
+++ src/pages/user-profile/user-profile.cmp.jsx
@@ -8,16 +8,26 @@ import { Avatar, Button } from 'antd';
 import { MdArrowBack } from 'react-icons/md';
 
 class UserProfile extends React.Component {
+ 
 	render() {
-		const { currentUser } = this.props;
+    const { currentUser } = this.props;
+    const userName = currentUser && currentUser.displayName.split(' ').slice(0, 1);
+    const avatarLetter = userName && userName[0].split('')[0].toUpperCase();
 		return (
 			<div className="user-profile">
 				<HeaderContainer>
 					<MdArrowBack className="back-button" onClick={() => this.props.history.goBack()} />
 					<h1>Your Profile</h1>
 				</HeaderContainer>
-				<div className="user-profile-display mt-30 flex-c-c p-30">
+        <div className="user-profile-display mt-30 flex-c-c p-30">
+        {currentUser&&currentUser.photoURL ? (
 					<Avatar className="avatar-picture" size={100} src={currentUser && currentUser.photoURL} />
+				) : (
+					<Avatar className="avatar-no-picture" size={100}>
+						<span style={{paddingRight:"1px", paddingBottom:"2px"}}>{avatarLetter}</span>
+					</Avatar>
+				)}
+					
 					<h2 className="mt-20 black">{currentUser && currentUser.displayName}</h2>
 					<h2 className="black">{currentUser && currentUser.email}</h2>
 					<Button type="primary" shape="round" size="large" className="mt-15">
